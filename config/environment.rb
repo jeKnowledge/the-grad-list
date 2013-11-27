43cd71adb49e950148a3e5d6e1b810e909777cc1
David Gomes <davidrafagomes@gmail.com>
2013-11-27 19:58:18 +0000

    Initial commit with user auth up and running with devise.

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+TheGradList2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheGradList2::Application.config.secret_key_base = '6572d8032070f7b4c63674c0ba7b7dc82fc48c26415899239a12badb1baaf136a85cde12f88c7e3475429bd402999a9f2a2bd74965440e6eefb7a0c705085242'
