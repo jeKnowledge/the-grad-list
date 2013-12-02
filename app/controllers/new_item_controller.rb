4748405d544cdc00140bad74a14b398e4ba67fda
David Gomes <davidrafagomes@gmail.com>
2013-12-02 22:09:44 +0000

    Added a check for item.content not being empty, should maybe be done client-side.

--- app/controllers/new_item_controller.rb
+++ app/controllers/new_item_controller.rb
@@ -5,8 +5,10 @@ class NewItemController < ApplicationController
 
   def added
     if request.POST.include? 'item_content' then
-      new_item = Item.new({:user => current_user, :content => params[:item_content]})
-      current_user.items.push(new_item)
+      if params[:item_content] != "" then
+        new_item = Item.new({:user => current_user, :content => params[:item_content]})
+        current_user.items.push(new_item)
+      end
     end
   end
 end
